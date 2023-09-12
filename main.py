47fbbcb2ed234dbf38d3c623bea3970133e3be5b
jai1910 <144827521+jai1910@users.noreply.github.com>
2023-09-12 22:28:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#"implement a recursive function to calculate the factorial of a given number."
+
+def fact(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact(n-1)
+
+number=int(input('enter a number:'))
+res=fact(number)
+print("the factorial of {} is {}.".format(number,res))
